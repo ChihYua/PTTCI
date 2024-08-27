e09f61a2791cce509cfb4edde1d1bd823464beff
ChihYua <friendly1774@gmail.com>
2024-08-27 12:06:23 +0800

    t0827

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import requests
 
 '''
 常數區塊
@@ -30,3 +30,15 @@ target_board = 'Stock'
 # 合併完整路徑
 target = BASE_URL + target_board + TARGET_PAGE + page_num + HTML_EXT
 
+
+def download_html(target, headers= HEADERS):
+    
+    #data = requests.get(target, headers=headers)
+    #return data
+    return requests.get(target, headers=headers)
+
+'''
+測試
+'''
+x=download_html(target)
+print(x.content)
